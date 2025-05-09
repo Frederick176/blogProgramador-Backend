79e358e5e23fec900646541d85d054c24e6b6bb3
Fredy Hernández <fhernandez-2023176@kinal.edu.gt>
2025-05-09 09:37:58 -0600

    agregado: user-validator.js para el funcionamiento del Backend

---
+++ src/helpers/db-validator.js
@@ -0,0 +1,22 @@
+import User from "../models/user.model.js"
+
+export const emailExist = async (email= "") => {
+    const exist = await User.findOne({email})
+    if(exist) {
+        throw new Error(`El email: ${email} ya fue registrado`)
+    }
+}
+
+export const userNameExist = async (username= "") => {
+    const exist = await User.findOne({username})
+    if(exist) {
+        throw new Error(`El username: ${username} ya fue registrado`)
+    }
+}
+
+export const userExist = async (uid = "") => {
+    const exist = await User.findById(uid)
+    if(!exist) {
+        throw new Error(`No existe un usuario con el ID: ${uid}`)
+    }
+}
