7d44221fc5cac8ccdaf0c9906bd3c4e8b03e9b9a
Frederick176 <fhernandez-2023176@kinal.edu.gt>
2025-05-19 16:11:16 -0600

    modificado: controller.js y routes.js para la funcionalidad de lo requerimientos

--- src/publications/publication.routes.js
+++ src/publications/publication.routes.js
@@ -1,6 +1,5 @@
 import { Router } from "express"
-import { createPublication, getPublications, getPublicationsByDateRecent, getPublicationsOld, getPublicationsByCourse, 
-    updatePublication, deletePublication} from "./publication.controller.js";
+import { createPublication, getPublications, getPublicationsByCourse, updatePublication, deletePublication} from "./publication.controller.js";
 import { createPublicationValidator, updatePublicationValidator, deletePublicationValidator, 
     getPublicationsByCourseValidator} from "../middlewares/publication-validator.js";
 
@@ -8,15 +7,11 @@ const router = Router();
 
 router.post("/add", createPublicationValidator, createPublication);
 
-router.get("/all", getPublications);
+router.get("/", getPublications);
 
 router.put("/update:id", updatePublicationValidator, updatePublication);
 
 router.delete("/delete:id", deletePublicationValidator, deletePublication);
-
-router.get("/recent", getPublicationsByDateRecent);
-
-router.get("/old", getPublicationsOld);
 
 router.get("/course:name", getPublicationsByCourseValidator, getPublicationsByCourse);
 
